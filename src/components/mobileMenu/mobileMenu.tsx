9081aabef0a49be9b6404f081a16e30098c0b4fa
nima <nekoueinima@protonmail.com>
2023-07-08 21:36:45 +0330

    [Feat]: add mobile navbar and make it responsive and add sharp package for production image optimization

--- src/components/mobileMenu/mobileMenu.tsx
+++ src/components/mobileMenu/mobileMenu.tsx
@@ -3,6 +3,8 @@
 import { FC, useState } from "react";
 import HamburgerButton from "../ui/hamburgerButton/hamburgerButton";
 import styles from "./mobileMenu.module.scss";
+import { navbarLinks } from "../navbar/navbar.data";
+import Link from "next/link";
 
 const MobileMenu: FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
@@ -12,6 +14,24 @@ const MobileMenu: FC = () => {
         isOpen={isMenuOpen}
         onClick={() => setIsMenuOpen((prev) => !prev)}
       />
+      <div
+        className={`${styles.mobile__menu} ${
+          isMenuOpen ? styles[`mobile__menu--open`] : ``
+        }`}
+      >
+        <ul className={styles.navigation}>
+          {navbarLinks.map((link, index) => (
+            <li
+              className={styles[`navigation__container`]}
+              key={`navbar-key-${index}`}
+            >
+              <Link className={styles[`navigation__link`]} href={link.href}>
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
