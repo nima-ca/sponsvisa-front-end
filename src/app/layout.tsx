ab8ab15749cc3eb54899cc7073bea19c54c815e3
nima <nekoueinima@protonmail.com>
2023-07-09 16:40:00 +0330

    [Feat]: add next-pwa and configure it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import Navbar from "@src/components/navbar/navbar";
 import Provider from "@src/components/providers/provider";
 import { vazirmatn } from "@src/styles/fonts/vazirmatn/vazirmatn";
+import PWAMetaTags from "@src/utils/pwaMetaTags";
 import "@styles/globals.scss";
 import { Metadata } from "next";
 
+const TITLE = `Sponsvisa`;
+const DESCRIPTION = `A platform to share the companies that supports relocation!`;
+
 export const metadata: Metadata = {
-  title: `Sponsvisa`,
-  description: `A platform to share the companies that supports relocation!`,
+  title: TITLE,
+  description: DESCRIPTION,
 };
 
 export default function RootLayout({
@@ -16,6 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={vazirmatn.className}>
+      <head>
+        <PWAMetaTags title={TITLE} description={DESCRIPTION} />
+      </head>
       <body>
         <Provider>
           <Navbar />
