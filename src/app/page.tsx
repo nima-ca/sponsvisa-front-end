7c03e9160e773e0c41637cb6dfb1e653b3d4d469
nima <nekoueinima@protonmail.com>
2023-07-12 02:20:18 +0330

    [Feat]: add home page content and image and add arrow rignt component from carbon system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
+import Image from "next/image";
 import styles from "./page.module.scss";
+import GirlImage from "@public/images/Girl-1.svg";
+import Link from "next/link";
+import { ArrowRightIcon } from "@src/components/icons/arrowRight";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <div className={`container ${styles.main__container}`}>
+        <Image
+          className={styles.main__image}
+          src={GirlImage}
+          alt="A girl Traveling"
+        />
+        <div className={styles[`content-box`]}>
+          <h1 className={styles.header}>Sponsvisa</h1>
+          <p className={styles.description}>
+            Sponsvisa is a platform to help you find the companies that offer
+            visa sponsorship! you can also contribute to the app by adding more
+            companies.
+          </p>
+          <Link className={styles.link} href="/companies">
+            Get Started <ArrowRightIcon />
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ src/components/icons/arrowRight.tsx
@@ -0,0 +1,18 @@
+import { SVGProps } from "react";
+
+export function ArrowRightIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 32 32"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="m18 6l-1.43 1.393L24.15 15H4v2h20.15l-7.58 7.573L18 26l10-10L18 6z"
+      ></path>
+    </svg>
+  );
+}
