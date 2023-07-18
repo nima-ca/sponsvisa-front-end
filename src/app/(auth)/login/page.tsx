8383e0b4f29c7d64d3e7ddd2d852ad3c0fbd68d2
nima <nekoueinima@protonmail.com>
2023-07-19 00:24:53 +0330

    [Feat]: add login page and formik and yup packages

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import styles from "./page.module.scss";
+
+const LoginPage: FC = () => {
+  return (
+    <main className={styles.container}>
+      <p>Login</p>
+    </main>
+  );
+};
+
+export default LoginPage;
