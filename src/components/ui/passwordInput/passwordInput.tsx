07b7fe23be2919c83555f1aa07f182fceabbae08
nima <nekoueinima@protonmail.com>
2023-07-20 18:07:54 +0330

    [Test]: add unit tests of the passwordInput component

--- src/components/ui/passwordInput/passwordInput.tsx
+++ src/components/ui/passwordInput/passwordInput.tsx
@@ -6,24 +6,37 @@ import { OpenEyeIcon } from "@src/components/icons/openEye";
 import Input from "@src/components/ui/input/input";
 import { useToggle } from "@src/hooks/useToggle/useToggle";
 import { FC } from "react";
-import { InputProps } from "../input/input.types";
+import { InputProps } from "@src/components/ui/input/input.types";
 import styles from "./passwordInput.module.scss";
+import {
+  PASSWORD_INPUT_CLOSED_EYE_TEST_ID,
+  PASSWORD_INPUT_ICON_BUTTON_TEST_ID,
+  PASSWORD_INPUT_OPEN_EYE_TEST_ID,
+  PASSWORD_INPUT_TEST_ID,
+} from "./passwordInput.constants";
 
 const PasswordInput: FC<Omit<InputProps, `type`>> = (props) => {
   const { state: show, toggle: toggleShow } = useToggle(false);
   return (
-    <InputGroup size="md">
+    <InputGroup size="md" data-testid={PASSWORD_INPUT_TEST_ID}>
       <Input {...props} type={show ? `text` : `password`} pr="3.5rem" />
       <InputRightElement width="3.5rem">
         <IconButton
           onClick={toggleShow}
           aria-label="Show Password"
           variant="link"
+          data-testid={PASSWORD_INPUT_ICON_BUTTON_TEST_ID}
           icon={
             show ? (
-              <ClosedEyeIcon className={styles.icon} />
+              <ClosedEyeIcon
+                data-testid={PASSWORD_INPUT_CLOSED_EYE_TEST_ID}
+                className={styles.icon}
+              />
             ) : (
-              <OpenEyeIcon className={styles.icon} />
+              <OpenEyeIcon
+                data-testid={PASSWORD_INPUT_OPEN_EYE_TEST_ID}
+                className={styles.icon}
+              />
             )
           }
         />
