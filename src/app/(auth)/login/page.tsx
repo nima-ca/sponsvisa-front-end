2fc5917b2a13e2086d4a0c8932d8f66503cbe040
nima <nekoueinima@protonmail.com>
2023-07-30 10:28:52 +0330

    [Fix]: add layout to login page for metadata error on build

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,17 +1,10 @@
 "use client";
 
-import { DESCRIPTION } from "@src/app/layout";
 import LoginForm from "@src/components/loginForm/loginForm";
 import { authContext } from "@src/context/authContext";
-import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { FC, useContext } from "react";
 import styles from "./page.module.scss";
-
-export const metadata: Metadata = {
-  title: `Sponsvisa | Login`,
-  description: DESCRIPTION,
-};
 
 const LoginPage: FC = () => {
   const auth = useContext(authContext);
